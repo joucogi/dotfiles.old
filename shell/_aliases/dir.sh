0ee0d65e6c307daeab47decc1abcdd4b64d2da82
Joel Coll Costa <joel.coll@gmail.com>
2020-11-17 16:38:57 +0100

    Alias to Work Dir, Efimatica and Codely

--- shell/_aliases/dir.sh
+++ shell/_aliases/dir.sh
@@ -8,3 +8,6 @@ alias ela="exa -la"
 # Jumps
 alias ~="cd ~"
 alias cdd
+alias wd=/Volumes/MACBOOK_EXT/Users/joelcollcosta
+alias codely=/Volumes/MACBOOK_EXT/Users/joelcollcosta/CodelyTV
+alias efi=/Volumes/MACBOOK_EXT/Users/joelcollcosta/Efimatica
