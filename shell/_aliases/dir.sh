64f83607704990cc52b27641ce992253dfc9758d
Joel Coll Costa <joel.coll@gmail.com>
2021-04-23 17:10:39 +0200

    Change working directory

--- shell/_aliases/dir.sh
+++ shell/_aliases/dir.sh
@@ -8,6 +8,6 @@ alias ela="exa -la"
 # Jumps
 alias ~="cd ~"
 alias cdd
-alias wd=/Volumes/MACBOOK_EXT/Users/joelcollcosta
-alias codely=/Volumes/MACBOOK_EXT/Users/joelcollcosta/CodelyTV
-alias efi=/Volumes/MACBOOK_EXT/Users/joelcollcosta/Efimatica
+alias wd=~/Documents
+alias codely=~/Documents/CodelyTV
+alias efi=~/Documents/Efimatica
