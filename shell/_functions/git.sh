7c45e45f04859ac74623551104a14a3607dfb69e
Joel Coll Costa <joel.coll@gmail.com>
2021-05-06 16:57:43 +0200

    Improve git flow aliases

---
+++ shell/_functions/git.sh
@@ -0,0 +1,70 @@
+function gffs() {
+    if [[ $# -gt 0 ]]
+    then
+        branch_name=$(1)
+    else
+        branch_name=$(openssl rand -base64 12)
+        branch_name=${branch_name//[\/+]/""}
+    fi
+
+    git flow feature start $branch_name
+}
+
+function testString() {
+    str="vsv/Lx+nNW5+0vg9E/"
+    echo ${str//[\/+]/""}
+}
+function gfff() {
+    tag=$(get_actual_tag)
+    git flow feature finish $tag
+}
+
+function gfrs() {
+    tag=$(get_new_tag)
+    git flow release start $tag
+}
+
+function gfrf() {
+    tag=$(get_actual_tag)
+    git flow release finish $tag
+}
+
+function gfhs() {
+    tag=$(get_new_tag)
+    git flow hotfix start $tag
+}
+
+function gfhf() {
+    tag=$(get_actual_tag)
+    git flow hotfix finish $tag
+}
+
+function get_actual_tag() {
+    echo $(gab | cut -d '/' -f 2)
+}
+
+function get_new_tag() {
+    today=$(date '+%Y%m%d')
+    version=1
+    tag="${today}.${version}"
+
+    result=$(git_tag_exists $tag)
+
+    while $result
+    do
+        ((version=version + 1))
+        tag="${today}.${version}"
+        result=$(git_tag_exists $tag)
+    done
+
+    echo $tag
+}
+
+function git_tag_exists() {
+    if [[ $(git tag | grep $1 | wc -l) -eq 0 ]]
+    then
+        echo false
+    else
+        echo true
+    fi
+}
